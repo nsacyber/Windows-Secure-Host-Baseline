f5e41f1135697572f5612d4b37c95df7971c08ef
iadgovuser1 <iadgovuser1@users.noreply.github.com>
2016-07-19 18:46:06 -0400

    change Disable-NetBIOS to be domain aware and to make a backup of previous value, add restore function to restore NetBIOS to previous value

--- Windows/Scripts/RemoveLegacyComponents.ps1
+++ Windows/Scripts/RemoveLegacyComponents.ps1
@@ -2,6 +2,67 @@
 #requires -Version 5
 Set-StrictMode -Version 5
 
+
+Function Test-IsDomainJoined() {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    Param()
+    #todo: function documentation
+    $computer = Get-WmiObject -Class 'Win32_ComputerSystem' | Select-Object PartOfDomain
+
+    return $computer.PartOfDomain
+}
+
+Function Test-RegistryValue() {
+<#
+    .SYNOPSIS  
+    Tests if a registry value exists.
+   
+    .DESCRIPTION
+    Tests if a registry value exists in the specified hive at the specified path.
+   
+    .PARAMETER Hive
+    The registry hive to check.
+   
+    .PARAMETER Path
+    The path of the registry key to check, not including the hive.
+   
+    .PARAMETER Name
+    The name of the registry value to check.
+     
+    .EXAMPLE
+    Test-RegistryValue -Hive 'hklm' -Path 'Software\Microsoft\Windows\CurrentVersion' -Name 'ProgramFilesDir'
+
+    .EXAMPLE
+    Test-RegistryValue 'hklm' 'Software\Microsoft\Windows\CurrentVersion' 'ProgramFilesDir'
+    #>
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    Param(
+        [Parameter(Position=0, Mandatory=$true, HelpMessage='The registry hive to check.')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Hive,
+      
+        [Parameter(Position=1, Mandatory=$true, HelpMessage='The path of the registry key, not including the hive.')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path,
+      
+        [Parameter(Position=2, Mandatory=$true, HelpMessage='The name of the registry value to check.')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+        $exists = $false
+
+    try {
+        Get-ItemProperty -Path ('{0}:\{1}' -f $Hive,$Path) -ErrorAction stop | Select-Object -ExpandProperty $Name -ErrorAction stop | Out-Null
+        $exists = $true
+    } catch [System.Management.Automation.PSArgumentException],[System.Management.Automation.ItemNotFoundException],[System.Management.Automation.ActionPreferenceStopException] {
+        $exists = $false
+    }
+
+    return $exists
+}
+
 Function Uninstall-Powershell2() {
     <#
     .SYNOPSIS
@@ -50,43 +111,88 @@ Function Disable-NetBIOS() {
     .DESCRIPTION
     Disable NetBIOS on all network interfaces regardless of whether the interface is active or not. NetBIOS is suspectible to man-in-the-middle attacks and is not required in a domain.
 
+    .PARAMETER -IncludeStandalone
+    Disable NetBIOS on standalone systems. By default only domain joined systems will have NetBIOS disabled.
+
     .EXAMPLE
     Disable-NetBIOS
+
+    .EXAMPLE
+    Disable-NetBIOS -IncludeStandalone
     #>
     [CmdletBinding()]
     [OutputType([void])]
-    Param()
+    Param(
+        [Parameter(Position=0, Mandatory=$false, HelpMessage='Disable NetBIOS on standalone systems')]
+        [switch]$IncludeStandalone    
+    )
 
     # ePO might have issues with disabling NetBIOS: https://kc.mcafee.com/corporate/index?page=content&id=KB76756 and https://kc.mcafee.com/corporate/index?page=content&id=KB56386
     # see also: https://support.microsoft.com/en-us/kb/313314
 
-    # this is only active adapters, not all possible ones
-    # does not appear to have a method to disable NetBIOS
-    #$interfaces = [System.Net.NetworkInformation.NetworkInterface[]]@([System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | Where-Object { $_.NetworkInterfaceType -notin @([System.Net.NetworkInformation.NetworkInterfaceType]::Tunnel,[System.Net.NetworkInformation.NetworkInterfaceType]::Loopback) })
+    if((Test-IsDomainJoined) -or $IncludeStandalone) {
+        $hive = 'hklm'
+        $keyPath = 'SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces'
+        $valueName = 'NetbiosOptions'
+        $previousValueName = 'Previous_NetbiosOptions'
 
-    #$adapters =  Get-WmiObject -Class 'Win32_NetworkAdapterConfiguration' 
+        # https://msdn.microsoft.com/en-us/library/windows/hardware/dn923165(v=vs.85).aspx
+        # 0 = Use DHCP server setting, 1 = Enabled, 2 = Disabled
+        $disabledValue = 2
 
-    # SetTcpipNetbios return 84 for most adapters since most are not real adapters (tunnels or loopback)
-    # https://msdn.microsoft.com/en-us/library/aa393601(v=vs.85).aspx
-    #$adapters | ForEach-Object {
-    #    $result = $_.SetTcpipNetbios($disabledValue)
-    #
-    #    Write-Verbose -Message ('{0} {1}' -f $_.Description, $_.SettingID)
-    #
-    #    if ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
-    #        if ($result -in @(0,1)) {
-    #         
-    #            Write-Verbose -Message ('Disabled Netbios on adapter named {0}' -f $_.Description)
-    #        }
-    #    }
-    #}
+        Get-ChildItem -Path ('{0}:\{1}' -f $hive,$keyPath) -Recurse | Where-Object { $_.GetValue($valueName) -ne $disabledValue } | ForEach-Object { 
+            $currentValue = $_.GetValue($valueName)
 
-    $path = 'hklm:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces'
-    $valueName = 'NetbiosOptions'
+            # create a backup value, if it doesn't exist, so that we can use it to restore the setting to the previous value
+            if (-not(Test-RegistryValue -Hive $hive -Path ('{0}\{1}' -f $keyPath,$_.PSChildName) -Name $previousValueName)) {        
+                Set-ItemProperty -Path ('{0}:\{1}\{2}' -f $hive,$keyPath,$_.PSChildName) -Name $previousValueName -Value $currentValue
+            }
 
-    # https://msdn.microsoft.com/en-us/library/windows/hardware/dn923165(v=vs.85).aspx
-    # 0 = Use DHCP server setting, 1 = Enabled, 2 = Disabled
-    $disabledValue = 2
+            Set-ItemProperty -Path ('{0}:\{1}\{2}' -f $hive,$keyPath,$_.PSChildName) -Name $valueName -Value $disabledValue 
+        }
+    }
+}
 
-    Get-ChildItem -Path $path -Recurse | Where-Object { $_.GetValue($valueName) -ne $disabledValue } | ForEach { Set-ItemProperty -Path ('{0}\{1}' -f $path,$_.PSChildName) -Name $valueName -Value $disabledValue }
+Function Restore-NetBIOS() {
+    <#
+    .SYNOPSIS
+    Restores NetBIOS to the previously saved state.
+
+    .DESCRIPTION
+    Restores NetBIOS to the previously saved state on all network interfaces regardless of whether the interface is active or not.
+
+    .PARAMETER -IncludeStandalone
+    Restore NetBIOS on standalone systems. By default only domain joined systems will have NetBIOS restored.
+
+    .EXAMPLE
+    Restore-NetBIOS
+
+    .EXAMPLE
+    Restore-NetBIOS -IncludeStandalone
+    #>
+    [CmdletBinding()]
+    [OutputType([void])]
+    Param(
+        [Parameter(Position=0, Mandatory=$false, HelpMessage='Restore NetBIOS on standalone systems')]
+        [switch]$IncludeStandalone    
+    )
+
+    if((Test-IsDomainJoined) -or $IncludeStandalone) {
+        $hive = 'hklm'
+        $keyPath = 'SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces'
+        $valueName = 'NetbiosOptions'
+        $previousValueName = 'Previous_NetbiosOptions'
+
+        Get-ChildItem -Path ('{0}:\{1}' -f $hive,$keyPath) -Recurse | Where-Object { $_.GetValue($previousValueName) -ne $null } | ForEach-Object { 
+            $currentValue = $_.GetValue($valueName)
+            $previousValue = $_.GetValue($previousValueName)
+
+            # create a backup value, if it doesn't exist, so that we can use it to restore the setting to the previous value
+            if ($currentValue-ne  $previousValue) {        
+                Set-ItemProperty -Path ('{0}:\{1}\{2}' -f $hive,$keyPath,$_.PSChildName) -Name $valueName -Value $previousValue
+            }
+
+            Remove-ItemProperty -Path ('{0}:\{1}\{2}' -f $hive,$keyPath,$_.PSChildName) -Name $previousValueName
+        }
+    }
 }
